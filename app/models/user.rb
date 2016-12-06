6b7718c3a1d4438135140682dd3a27a093a43e11
shinoman <hoshifuruokani@gmail.com>
2016-12-06 10:29:01 +0900

    バックグラウンドでの画像保存

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base
-    mount_uploader :image, UserImageUploader
     # Image upload
     mount_uploader :image, UserImageUploader
     # - 追加 -
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161205151041) do
+ActiveRecord::Schema.define(version: 20161206011242) do
 
   create_table "favorite_microposts", force: :cascade do |t|
     t.integer "user_id"
